6f36778742659cb10bc9c4da4e6987bb9093feda
JereUR <jeremias.jdv@gmail.com>
2024-04-19 20:51:37 -0300

    feat(Root): Chart component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import General from '@/components/Cards/General'
+import { Card } from '@/components/ui/card'
 
 export default function Home() {
   return (
-    <div>
-      Homepage
-      <Button variant={'outline'}>Click me</Button>
+    <div className="grid grid-cols-2 gap-[32px]">
+      <General />
+      {/* <div className="grid grid-cols-2">
+        <Card />
+        <Card />
+      </div> */}
     </div>
   )
 }
